e3cf5d97db94c5a5339407c69a0eed99df841add
itsmearpan1@gmail.com <arpanbhandari@Arpans-MacBook-Air.local>
2023-12-22 13:42:12 +0545

    Added comments to end points types

--- src/types/app/server/endPoints.ts
+++ src/types/app/server/endPoints.ts
@@ -7,11 +7,6 @@ type END_POINT_DETAIL = {
   hasMiddleWare: boolean;
 };
 
-type AVAILABLE_ROUTE = {
-  filePath: string;
-  hasMiddleWare: boolean;
-};
-
 type END_POINTS = Array<END_POINT_DETAIL>;
 
-export { END_POINTS, AVAILABLE_ROUTE, END_POINT_DETAIL };
+export { END_POINTS, END_POINT_DETAIL };
