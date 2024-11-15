a85585fc4ebe79157d9b1c2a75e296fd576c72b5
Arpan Bhandari <itsmearpan1@gmail.com>
2024-11-15 14:43:10 -0600

    Removed unneccsary files

--- src/core/server/type.ts
+++ src/core/server/type.ts
@@ -7,14 +7,20 @@ declare namespace http {
 }
 interface REQUEST extends IncomingMessage {}
 
-// type RESPONSE<T> = {
-//   status: number;
-//   message?: string;
-//   data?: T;
-//   error?: string | JOOR_ERROR;
-//   cookies?: { [key: string]: string };
-//   headers?: { [key: string]: string };
-//   session?: string;
-// };
+type JOOR_ERROR = {
+  code?: string;
+  message?: string;
+  details?: string;
+  timeStamp?: Date;
+};
 
-export { REQUEST };
+type RESPONSE<T> = {
+  status: number;
+  message?: string;
+  data?: T;
+  error?: string | JOOR_ERROR;
+  cookies?: { [key: string]: string };
+  headers?: { [key: string]: string };
+  session?: string;
+};
+export { REQUEST, RESPONSE };
