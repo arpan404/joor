257a38914d19d859be94809aa24023b0f0e8aedd
Arpan Bhandari <itsmearpan1@gmail.com>
2024-11-15 13:02:46 -0600

    Added jsdocs in configuration class

--- src/core/configuration/index.ts
+++ src/core/configuration/index.ts
@@ -6,19 +6,44 @@ import Jrror from "../error";
 import JOOR_CONFIG from "./type";
 
 /**
- * Class to work with configuration file.
+ * Class to handle application configuration files.
+ *
+ * This class provides methods to load and access configuration data from a file.
+ * It ensures that configuration is loaded only once and throws custom errors in case of issues.
  *
  * @example
- * const config = Configuration.getConfig();
+ * const config = await new Configuration().getConfig();
  * console.log(config.port);
- *
- *
  */
-
 class Configuration {
+  /**
+   * The relative path to the configuration file.
+   * @type {string}
+   * @private
+   */
   private static configFile: string = joorData.configFile;
+
+  /**
+   * The loaded configuration data.
+   * Null if the configuration has not yet been loaded.
+   * @type {JOOR_CONFIG | null}
+   * @private
+   */
   private static configData: JOOR_CONFIG | null = null;
 
+  /**
+   * Loads the configuration file and parses its contents.
+   *
+   * This method reads the configuration file, parses it as JSON, and stores the result
+   * in `Configuration.configData`. It ensures that the configuration is only loaded once.
+   *
+   * @throws {Jrror} Throws:
+   * - "config-1" if the configuration is already loaded.
+   * - "config-p1" if the file is not found.
+   * - "config-p2" if there is a syntax error in the configuration file.
+   * - "config-p3" for other unknown errors.
+   * @private
+   */
   private async loadConfig(): Promise<void> {
     if (Configuration.configData !== null) {
       throw new Jrror("config-1");
@@ -43,6 +68,15 @@ class Configuration {
     }
   }
 
+  /**
+   * Retrieves the configuration data.
+   *
+   * If the configuration data has not been loaded, it calls `loadConfig` to load it.
+   *
+   * @returns {Promise<JOOR_CONFIG>} The configuration data.
+   * @throws {Jrror} Throws "config-p3" if configuration loading fails unexpectedly.
+   * @protected
+   */
   protected async getConfig(): Promise<JOOR_CONFIG> {
     if (Configuration.configData === null) {
       await this.loadConfig();
