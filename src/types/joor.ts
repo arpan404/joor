064aecbbd2bc32f8cd4c15442a5ff78728bea0ba
Arpan Bhandari <itsmearpan1@gmail.com>
2025-02-03 09:32:04 -0600

    #chore: FOrmatting

--- src/types/joor.ts
+++ src/types/joor.ts
@@ -1,11 +1,11 @@
 import { ROUTE_HANDLER } from '@/types/route';
 
 type GLOBAL_MIDDLEWARES = Array<ROUTE_HANDLER>;
-type SERVE_FILES_CONFIG = {
+interface SERVE_FILES_CONFIG {
   routePath: string;
   folderPath: string;
   stream: boolean;
   download: boolean;
-};
+}
 
 export { GLOBAL_MIDDLEWARES, SERVE_FILES_CONFIG };
