ea003a888dff0a9c548f0a2a852f43fa7a1da8e4
Arpan Bhandari <itsmearpan1@gmail.com>
2025-02-06 13:53:44 -0600

    refactor:  function now accepts data as object

--- src/enhanchers/redirect.ts
+++ src/enhanchers/redirect.ts
@@ -1,36 +1,37 @@
 import JoorResponse from '@/core/response';
 
 /**
- * Redirects the client to a specified path with an appropriate HTTP status code.
- *
- * This function sets the `Location` header in the response to the given path,
- * and determines the HTTP status code based on whether the redirect is permanent
- * (301) or temporary (302). The status code and headers are then applied to the
- * JoorResponse object, which is returned to the client.
- *
- * @param {string} path - The URL path to redirect the client to.
- * @param {true} permanent - A boolean indicating if the redirect should be permanent.
- *   If `true`, the redirect will be permanent (HTTP status code 301).
- *   If `false` (default), the redirect will be temporary (HTTP status code 302).
- *
- * @returns {Promise<JoorResponse>} A promise that resolves to a JoorResponse object,
- *   which includes the correct status code (301 or 302) and a `Location` header
- *   pointing to the specified path.
+ * Redirects the client to the given path, when returned from a route handler or middleware.
+ * @param path The URL to redirect to.
+ * @param permanent Whether the redirect is permanent (301) or temporary (302). Defaults to true.
+ * @returns A JoorResponse with the appropriate status code and Location header.
  *
  * @example
- * ```typescript
- * const response = await redirect('/new-path', true);
- * return response;
- * // or
- * return redirect('/new-path', true);
- * // This will issue a permanent redirect (301) to '/new-path'.
- * // The response will have status 301 and the Location header set to '/new-path'.
+ * ```ts
+ * // Redirect to a new path
+ * router.get('/old-path', async () => {
+ *  return redirect({
+ *    path: '/new-path',
+ *    permanent: true,
+ *  });
+ * });
+ *
+ * // Redirect to a new path temporarily
+ * router.get('/temporary-path', async () => {
+ *  return redirect({
+ *    path: '/new-path',
+ *    permanent: false,
+ *  });
+ * });
  * ```
  */
-export default async function redirect(
-  path: string,
-  permanent: boolean = true
-): Promise<JoorResponse> {
+export default async function redirect({
+  path,
+  permanent = true,
+}: {
+  path: string;
+  permanent?: boolean;
+}): Promise<JoorResponse> {
   const response = new JoorResponse();
   const statusCode = permanent ? 301 : 302;
   response.setStatus(statusCode).setHeaders({ Location: path });
