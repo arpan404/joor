d06f8826080bab7419d8af165c2042870e954f9c
Arpan Bhandari <itsmearpan1@gmail.com>
2025-02-07 21:52:15 -0600

    feat: Websocket works using app.sockets but refactoring is needed

--- dev/playgrounds/ground0/socket.js
+++ dev/playgrounds/ground0/socket.js
@@ -1,27 +1,27 @@
-const { io } = require("socket.io-client");
+const { io } = require('socket.io-client');
 
 // Connect to the WebSocket server
-const socket = io("http://localhost:3000");
+const socket = io('http://localhost:3000');
 
 // When connected
-socket.on("connect", () => {
+socket.on('connect', () => {
   console.log(`Connected to server with ID: ${socket.id}`);
 
   // Send a message to the server
-  socket.emit("message", "Hello from client!");
+  socket.emit('message', 'Hello from client!');
 
   // Listen for responses
-  socket.on("response", (data) => {
-    console.log("Server says:", data);
+  socket.on('response', (data) => {
+    console.log('Server says:', data);
   });
 
   // Send a new message every 3 seconds
-  // setInterval(() => {
-  //   socket.emit("message", "Ping from client!");
-  // }, 1000);
+  setInterval(() => {
+    socket.emit("message", "Ping from client!");
+  }, 1000);
 });
-
+socket.emit('message', 'Hello from client!');
 // Handle disconnection
-socket.on("disconnect", () => {
-  console.log("Disconnected from server.");
+socket.on('disconnect', () => {
+  console.log('Disconnected from server.');
 });
