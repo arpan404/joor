ec7cb04de7c68129084f8dfe5fad2c8ef7d17f3f
Arpan Bhandari <itsmearpan1@gmail.com>
2025-02-12 10:19:26 -0600

    chore: initiate the refactoring of response object; rearchitecturing

---
+++ src/core/response/test.ts
@@ -0,0 +1,51 @@
+import { JoorRequest } from '@/types/request';
+import { ServerResponse } from 'node:http';
+import logger from '@/helpers/joorLogger';
+import Jrror from '@/core/error';
+import JoorError from '@/core/error/JoorError';
+
+/** Extends ServerResponse with added functionality for better DX  */
+class JoorReponse extends ServerResponse {
+  constructor(req: JoorRequest) {
+    super(req);
+  }
+  public status(code: number): JoorReponse {
+    try {
+      if (typeof code !== 'number') {
+        throw new Jrror({
+          code: 'response-status-invalid',
+          message: `Status must be a number, but ${typeof code} was provided.`,
+          type: 'error',
+          docsPath: '/response',
+        });
+      }
+      if (code < 100 || code > 599) {
+        throw new Jrror({
+          code: 'response-status-invalid',
+          message: `Status must be a number, but ${typeof code} was provided.`,
+          type: 'error',
+          docsPath: '/response',
+        });
+      }
+      this.statusCode = code;
+    } catch (error: unknown) {
+      if (error instanceof Jrror || error instanceof JoorError) {
+        error.handle();
+      } else {
+        logger.error(error);
+      }
+    }
+    return this;
+  }
+
+  public json(data: unknown): void {
+    this.setHeader('Content-Type', 'application/json');
+    this.write(JSON.stringify(data));
+    this.end();
+  }
+  public send(): void {
+    this.end();
+  }
+}
+
+export default JoorReponse;
